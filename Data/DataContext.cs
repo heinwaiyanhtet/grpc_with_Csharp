3bc20802eaab1f834cf1ec0ebb52984df9cc101d
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2023-09-06 22:27:44 +0630

    data context is added

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,1 +1,15 @@
 // using Microsoft.EntityFrameworkCore;
+using System.Data.Entity;
+using BlogGrpc.Models;
+
+namespace ToDoGrpc.Data;
+
+public class AppDbContext : DbContext
+{
+    // public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    // {
+        
+    // }
+
+    public DbSet<Blog> blogs { get; set; } = default!;
+}
--- Model/Blog.cs
+++ Model/Blog.cs
@@ -5,16 +5,19 @@ namespace BlogGrpc.Models;
 
 public class Blog
 {
+    [Key]
     public Guid Id {get;set;} =  Guid.NewGuid();
 
     [Required]
     [MaxLength(255)]
     [MinLength(4)]
     public string Title {get;set;}
-    
+
+
     [Required]
     [MinLength(200)]
     public string Description {get;set;}    
+
     public string Author {get;set;} 
 
 }   
